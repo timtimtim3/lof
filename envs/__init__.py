25fa37e70dbb48ef36e0050996a4f7f392a1245f
Kuroo <dkuric20@gmail.com>
2023-11-09 12:33:23 +0100

    Add DoubleSlit env

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,6 +1,19 @@
 import gym
 
 gym.envs.register(
+    id='DoubleSlit-v0',
+    entry_point='envs.grid_envs:DoubleSlit',
+    max_episode_steps=1000,
+)
+
+gym.envs.register(
+    id='DoubleSlitEval-v0',
+    entry_point='envs.grid_envs:DoubleSlit',
+    max_episode_steps=1000,
+)
+
+
+gym.envs.register(
     id='DeliveryMini-v0',
     entry_point='envs.grid_envs:DeliveryMini',
     max_episode_steps=200,
